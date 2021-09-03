61f6bc43dabed1ac255f38398c04fffcae82c7fe
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-09-04 01:41:04 +0430

    Query list of books

--- README.md
+++ README.md
@@ -23,3 +23,16 @@ Run the app with the following command:
 node app.js
 ```
 Then, load [`http://localhost:3000`](http://localhost:3000) in a browser to see the output.
+
+## GraphQL Sample Queries
+
+Get list of all books:
+
+```console
+{
+  books {
+    id
+    name
+  }
+}
+```
--- app.js
+++ app.js
@@ -1,33 +1,77 @@
-const express = require('express')
-const { graphqlHTTP } = require('express-graphql');
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
 
-const { 
+const {
   GraphQLSchema,
   GraphQLObjectType,
   GraphQLString,
-} = require('graphql');
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNonNull,
+} = require("graphql");
 
-// Construct a schema using GraphQL object types
+const authors = [
+  { id: 1, name: "J. K. Rowling" },
+  { id: 2, name: "J. R. R. Tolkien" },
+  { id: 3, name: "Brent Weeks" },
+];
+
+const books = [
+  { id: 1, name: "Harry Potter and the Chamber of Secrets", authorId: 1 },
+  { id: 2, name: "Harry Potter and the Prisoner of Azkaban", authorId: 1 },
+  { id: 3, name: "Harry Potter and the Goblet of Fire", authorId: 1 },
+  { id: 4, name: "The Fellowship of the Ring", authorId: 2 },
+  { id: 5, name: "The Two Towers", authorId: 2 },
+  { id: 6, name: "The Return of the King", authorId: 2 },
+  { id: 7, name: "The Way of Shadows", authorId: 3 },
+  { id: 8, name: "Beyond the Shadows", authorId: 3 },
+];
+
+// The book type nested in the root query
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  description: "This represents a book written by an author",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    authorId: { type: GraphQLNonNull(GraphQLInt) },
+  }),
+});
+
+// The root (main) query of GraphQL
+const RootQueryType = new GraphQLObjectType({
+  name: "Query",
+  description: "Root query",
+  fields: () => ({
+    books: {
+      type: new GraphQLList(BookType),
+      description: "List of all books",
+      resolve: () => books,
+    },
+  }),
+});
+
+// The GraphQL schema
 const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'HelloWorld',
-    fields: () => ({
-      hello: {
-        type: GraphQLString,
-        resolve: () => "Hello world!",
-      }
-    })
+  query: RootQueryType,
+});
+
+const app = express();
+
+// To avoid deployment problems in evironments like Heroku,
+// the port is first set by process.env.PORT environment variable
+const port = process.env.PORT || 3000;
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
   })
-})
-
-const app = express()
-const port = process.env.PORT || 3000
-
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  graphiql: true,
-}));
+);
 
 app.listen(port, () => {
-  console.log(`Running a GraphQL API server at http://localhost:${port}/graphql`)
-})
+  console.log(
+    `Running a GraphQL API server at http://localhost:${port}/graphql`
+  );
+});
